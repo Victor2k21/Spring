650789789810a390d185cb9c0bf1d5eb6802f580
Victor2k21 <davidenkovitya@gmail.com>
2024-02-10 21:52:52 +0300

    add Get requests

---
+++ src/main/java/com/merion/spring/book/service/BookService.java
@@ -0,0 +1,41 @@
+package com.merion.spring.book.service;
+
+import com.merion.spring.book.entity.BookEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+public class BookService {
+    static List<BookEntity> bookStorage = new ArrayList<>();
+
+    public BookService(){
+        fillStorage();
+    }
+
+    public void fillStorage(){
+        Random random = new Random();
+        for (int i = 0; i < 100; i++) {
+            BookEntity book = new BookEntity();
+            book.setId(i);
+            book.setTitle("Book #" + random.nextInt(100,999));
+            book.setDescription("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore" +
+                    " veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur" +
+                    " magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non" +
+                    " numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit" +
+                    " laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum" +
+                    " qui dolorem eum fugiat quo voluptas nulla pariatur?");
+            bookStorage.add(book);
+        }
+    }
+    public List<BookEntity> all(){
+        return bookStorage;
+    }
+
+    public Optional<BookEntity> byId(Integer id){
+        return bookStorage.stream().filter( (book -> book.getId().equals(id))).findFirst();
+    }
+}
