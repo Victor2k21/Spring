6d3e0dcad60f2a35eda88054b0d792d222752bcf
Victor2k21 <davidenkovitya@gmail.com>
2024-02-18 16:00:26 +0300

    add Patch request

--- src/main/java/com/merion/spring/book/controller/BookApiController.java
+++ src/main/java/com/merion/spring/book/controller/BookApiController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.awt.print.Book;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class BookApiController {
@@ -49,5 +50,10 @@ public class BookApiController {
     @DeleteMapping("/api/v1/book/{id}")
     public Boolean delete(@PathVariable Integer id){
         return bookService.delete(id);
+    }
+
+    @PatchMapping("/api/v1/book/{id}")
+    public BookEntity path(@PathVariable Integer id, @RequestBody Map<String, String> fields){
+        return bookService.editPart(id, fields).orElseThrow(ResourceNotFoundException::new);
     }
 }
